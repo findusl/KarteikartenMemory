dac78e6c03ae75cf51f07f8d40a29d6b9c2fc5c5
findusl <sebastian@lehrbaum.de>
2015-02-21 17:28:01 +0100

    Create Card and Main Menu implemented

--- src/de/lehrbaum/view/MemoryCardView.java
+++ src/de/lehrbaum/view/MemoryCardView.java
@@ -5,6 +5,7 @@ import java.awt.Color;
 import javax.swing.JButton;
 import javax.swing.border.LineBorder;
 
+@SuppressWarnings("serial")
 public class MemoryCardView extends JButton {
 	
 	public MemoryCardView() {
